de385938064aefdf9175bd549d86747a749403c3
Dani <Dani@Dani.Dani>
2019-04-15 18:38:36 +0200

    Server create

---
+++ src/java/Server/GameProtocol.java
@@ -0,0 +1,12 @@
+package Server;
+
+public class GameProtocol {
+    public String processInput(String theInput) {
+
+    String theOutput = null;
+
+    theOutput = theInput;
+
+    return theOutput;
+}
+}
